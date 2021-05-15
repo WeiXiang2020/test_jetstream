83d31d69923c22fb08d3bd9290b136afad98c107
pusean <3a732094@gm.student.ncut.edu.tw>
2021-05-15 16:24:40 +0800

    追加js 與png 的asset

--- resources/views/teacher/layouts/main.blade.php
+++ resources/views/teacher/layouts/main.blade.php
@@ -10,8 +10,8 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
-    <link href="{{asset('assets/img/apple-touch-icon.png" rel="apple-touch-icon')}}">
+    <link href="{{asset('assets/img/favicon.png)}}')}}" rel="icon">
+    <link href="{{asset('assets/img/apple-touch-icon.png)}}" rel="apple-touch-icon')}}">
 
     <!-- Google Fonts -->
     <link href="{{asset('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i')}}" rel="stylesheet">
@@ -40,9 +40,9 @@
 <header id="header" class="fixed-top header-transparent">
     <div class="container d-flex align-items-center justify-content-between">
 
-        <h1 class="logo"><a href="index.html">Baker</a></h1>
+        <h1 class="logo"><a href="index">Baker</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <!-- <a href="index.html" class="logo"><img src="{{asset(assets/img/logo.png)}}" alt="" class="img-fluid"></a>-->
 
         <nav id="navbar" class="navbar">
             <ul>
@@ -170,15 +170,15 @@
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
-<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-<script src="assets/vendor/php-email-form/validate.js"></script>
-<script src="assets/vendor/purecounter/purecounter.js"></script>
-<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="{{asset(assets/vendor/bootstrap/js/bootstrap.bundle.min.js)}}"></script>
+<script src="{{asset(assets/vendor/glightbox/js/glightbox.min.js)}}"></script>
+<script src="{{asset(assets/vendor/isotope-layout/isotope.pkgd.min.js)}}"></script>
+<script src="{{asset(assets/vendor/php-email-form/validate.js)}}"></script>
+<script src="{{asset(assets/vendor/purecounter/purecounter.js)}}"></script>
+<script src="{{asset(assets/vendor/swiper/swiper-bundle.min.js)}}"></script>
 
 <!-- Template Main JS File -->
-<script src="assets/js/main.js"></script>
+<script src="{{asset(assets/js/main.js)}}"></script>
 
 </body>
 
